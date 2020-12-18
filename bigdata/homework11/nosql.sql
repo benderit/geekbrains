892f1809e0e64966b383d735fa444779ba128157
benderit <37499409+benderit@users.noreply.github.com>
2020-12-18 16:51:35 +0300

    Add files via upload

---
+++ bigdata/homework11/nosql.sql
@@ -0,0 +1,22 @@
+Практическое задание по теме “NoSQL”
+1. В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов.
+sadd ip1 date1 date2 date3
+sinter ip1
+
+2. При помощи базы данных Redis решите задачу поиска имени пользователя по электронному адресу и наоборот, поиск электронного адреса пользователя по его имени.
+hset get_email_by_name hiro 89046397102@mail.ru
+hset get_name_by_email 89046397102@mail.ru hiro
+
+hget get_email_by_name hiro
+hget get_name_by_email 89046397102@mail.ru
+
+
+
+3. Организуйте хранение категорий и товарных позиций учебной базы данных shop в СУБД MongoDB.
+use test2
+db.products.insert({
+    name: "Intel Core i3-8100",
+    description: "Процессор для настольных персональных компьютеров, основанных на платформе Intel.",
+    price: 7890.00,
+    catalog: "Процессоры"
+})
